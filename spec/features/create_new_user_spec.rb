1545a7487bd725bbd42d033d1df3af500119a5f2
Gabe Pressman <pressman4747@gmail.com>
2016-02-10 18:46:07 -0500

    added database cleaner

---
+++ spec/features/create_new_user_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "create new user" do 
+  scenario "successfully" do
+    visit new_user_path
+
+    fill_in "user_email", with: "a@b.com"
+    fill_in "user_password", with: "password"
+    fill_in "user_phone_number", with: "610-331-4327"
+    fill_in "user_first_name", with: "Gabe"
+    fill_in "user_last_name", with: "Pressman"
+    
+
+    expect(page).to have_css "p", text: "a@b.com"
+  end
+end
