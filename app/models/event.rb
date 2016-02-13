5372d1b520c87a0f57a2d145e4005378da0a76a6
Gabe Pressman <pressman4747@gmail.com>
2016-02-13 12:55:46 -0500

    added validations for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ActiveRecord::Base
+  validates  :name, :address, :workers, :time_start, :time_end, presence: {message: "Must be filled"} 
   has_and_belongs_to_many :users
 
   
 end
+
+
