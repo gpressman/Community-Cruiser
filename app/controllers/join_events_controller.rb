843e32ba320df5d5b94d62a295eece23b2aee204
Gabe Pressman <pressman4747@gmail.com>
2016-02-13 10:51:07 -0500

    fixed formatting on new event form

--- app/controllers/join_events_controller.rb
+++ app/controllers/join_events_controller.rb
@@ -1,12 +1,6 @@
 class JoinEventsController < ApplicationController
  
   def create
-  	@event = Event.find(params[:id])
-  	@user = current_user
-  	@event.users << @user
-  	@event.workers -= 1
-  	@event.save
-  	@user.save
-    redirect_to @event
+  	current_user.join
   end
 end
